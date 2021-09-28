a00074e100c8aaa2ca6548b2737730fc59d5e50b
Onur GURSOY <onur.gursoy@siemens.com>
2021-09-28 18:59:58 +0300

    initial commit

---
+++ springcloudgateway/src/main/java/com/muhendisbey/springcloudgateway/Springcloudgateway.java
@@ -0,0 +1,15 @@
+package com.muhendisbey.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloudgateway
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Springcloudgateway.class, args);
+    }
+
+}
---
+++ springcloudgateway/src/test/java/com/muhendisbey/springcloudgateway/SpringcloudgatewayZuulproxyExampleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.muhendisbey.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudgatewayZuulproxyExampleApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}

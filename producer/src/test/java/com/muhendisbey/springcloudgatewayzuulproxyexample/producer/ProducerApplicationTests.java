81554327f8df57e3c1150a5fe3d3d1700f37f0c1
Onur GURSOY <onur.gursoy@siemens.com>
2021-10-01 17:39:28 +0300

    all project files were added

---
+++ producer/src/test/java/com/muhendisbey/springcloudgatewayzuulproxyexample/producer/ProducerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.muhendisbey.springcloudgatewayzuulproxyexample.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
